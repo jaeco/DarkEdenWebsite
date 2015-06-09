f5ccad4ed854ba0721e26ee9d737a9451d2b3542
Dia Brown <dbrown1903@outlook.com>
2015-06-09 17:46:22 -0600

    NEWS & EVENTS
    The news and events page is completed, all is left is correcting it in
    the home page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DarkEdenWebsite.Models;
 
 namespace DarkEdenWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        private Update update = new Update();
+        private List<Update> ups = new List<Update> { new Update { Date = new DateTime(2015,5,3) , Title = "New Changes", Description = "We have changed a few of the layouts on the page. But nothing too crazy has changed. Carry on!", TypeOfUpdate = UpdateType.News},
+                                                  new Update { Date = new DateTime(2015,6,1), Title = "Vamp Marathon", Description = "We are hosting a Vamp Marathon where all vampires in game can earn items based on actions.", TypeOfUpdate = UpdateType.Events},
+                                                  new Update { Date = new DateTime(2015,5,3) , Title = "New Changes", Description = "We have changed a few of the layouts on the page. But nothing too crazy has changed. Carry on!", TypeOfUpdate = UpdateType.News},
+                                                  new Update { Date = new DateTime(2015,5,3) , Title = "New Changes", Description = "We have changed a few of the layouts on the page. But nothing too crazy has changed. Carry on!", TypeOfUpdate = UpdateType.News},
+                                                  new Update { Date = new DateTime(2015,5,3) , Title = "New Changes", Description = "We have changed a few of the layouts on the page. But nothing too crazy has changed. Carry on!", TypeOfUpdate = UpdateType.News},
+                                                  new Update { Date = new DateTime(2015,5,3) , Title = "New Changes", Description = "We have changed a few of the layouts on the page. But nothing too crazy has changed. Carry on!", TypeOfUpdate = UpdateType.News},
+                                                  new Update { Date = new DateTime(2015,5,3) , Title = "New Changes", Description = "We have changed a few of the layouts on the page. But nothing too crazy has changed. Carry on!", TypeOfUpdate = UpdateType.News},
+                                                  new Update { Date = new DateTime(2015,5,3) , Title = "New Changes", Description = "We have changed a few of the layouts on the page. But nothing too crazy has changed. Carry on!", TypeOfUpdate = UpdateType.News},
+                                                  new Update { Date = new DateTime(2015,6,1), Title = "Vamp Marathon", Description = "We are hosting a Vamp Marathon where all vampires in game can earn items based on actions.", TypeOfUpdate = UpdateType.Events},
+                                                  new Update { Date = new DateTime(2015,6,1), Title = "Vamp Marathon", Description = "We are hosting a Vamp Marathon where all vampires in game can earn items based on actions.", TypeOfUpdate = UpdateType.Events},
+                                                  new Update { Date = new DateTime(2015,6,1), Title = "Vamp Marathon", Description = "We are hosting a Vamp Marathon where all vampires in game can earn items based on actions.", TypeOfUpdate = UpdateType.Events},
+                                                  new Update { Date = new DateTime(2015,6,1), Title = "Vamp Marathon", Description = "We are hosting a Vamp Marathon where all vampires in game can earn items based on actions.", TypeOfUpdate = UpdateType.Events},
+                                                  new Update { Date = new DateTime(2015,6,1), Title = "Vamp Marathon", Description = "We are hosting a Vamp Marathon where all vampires in game can earn items based on actions.", TypeOfUpdate = UpdateType.Events},
+                                                  new Update { Date = new DateTime(2015,6,1), Title = "Vamp Marathon", Description = "We are hosting a Vamp Marathon where all vampires in game can earn items based on actions.", TypeOfUpdate = UpdateType.Events},
+                                                 };
+
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            foreach (var i in ups)
+            {
+                update.TheListOfUpdates.Add(i);
+            }
+
+            return View(update);
         }
 
         public ActionResult Market()
@@ -21,7 +45,11 @@ namespace DarkEdenWebsite.Controllers
 
         public ActionResult NewsEvents()
         {
-            return View();
+            foreach (var i in ups)
+            {
+                update.TheListOfUpdates.Add(i);
+            }
+            return View(update);
         }
 
         public ActionResult Info()
